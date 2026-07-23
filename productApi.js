350eda6f3b26ea05d9f217250444e5fc3d6f17b0
Ricky Bruce Amissah <rbamissah001@st.ug.edu.gh>
2026-07-23 09:16:06 -1200

    Add product inventory data and update API routes

--- productApi.js
+++ productApi.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
-const express = require("express")
+const express = require("express");
+const productItems = require("./productInventory");
 const productApi = express();
 
 const PORT = process.env.PORT ;
+const productItems = require("productInventory.js")
 
 productApi.use(express.json())  // This is my middleware
 
-productApi.get("/product", ()=>{
-    // Do something
+productApi.get("/inventory", ()=>{
+    res.status(200).json(productItems);
 })
 
 productApi.post("/product", ()=>{
